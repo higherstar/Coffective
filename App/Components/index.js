d77be45c21317284037f405fc008a81f691bbbb3
Serhii Levchenko <sleva13@gmail.com>
2017-07-26 13:56:53 +0200

    remove old components

--- App/Components/index.js
+++ App/Components/index.js
@@ -2,9 +2,6 @@ import Button from './Button'
 import Txt from './Txt'
 import TextView from './TextView'
 import Input from './Input'
-import ChecklistGroup from './ChecklistGroup'
-import Article from './Article'
-import ChecklistTab from './ChecklistTab'
 import Drawer from './Drawer'
 import Loader from './Loader'
 import SafeDataInfo from './SafeDataInfo'
@@ -14,9 +11,6 @@ export {
   Txt,
   TextView,
   Input,
-  ChecklistGroup,
-  Article,
-  ChecklistTab,
   Drawer,
   Loader,
   SafeDataInfo,

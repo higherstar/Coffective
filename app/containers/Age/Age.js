4151833f2ff3b5f9df33a3f52b8438a2872e33bd
Serhii Levchenko <sleva13@gmail.com>
2018-04-13 18:07:42 +0200

    replace svg icons to url, change ages icons

--- app/containers/Age/Age.js
+++ app/containers/Age/Age.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Image, ScrollView, TouchableOpacity, View } from 'react-native'
-import { Txt } from '../../components'
+import { Txt, Img } from '../../components'
 import I18n from 'react-native-i18n'
 import s from './AgeStyles'
 import { Images, Metrics } from '../../themes'
@@ -13,32 +13,32 @@ const ages = [
   {
     value: '>15',
     label: 'Under 15',
-    image: Images.mother,
+    image: Images.ages['under15'],
   },
   {
     value: '16-18',
     label: '16-18',
-    image: Images.mother,
+    image: Images.ages['16-18'],
   },
   {
     value: '18-25',
     label: '18-25',
-    image: Images.mother,
+    image: Images.ages['18-25'],
   },
   {
     value: '25-35',
     label: '25-35',
-    image: Images.mother,
+    image: Images.ages['25-35'],
   },
   {
     value: '35-45',
     label: '35-45',
-    image: Images.mother,
+    image: Images.ages['35-45'],
   },
   {
     value: '50+',
     label: '50+',
-    image: Images.mother,
+    image: Images.ages['50+'],
   },
 ]
 
@@ -78,7 +78,7 @@ class Age extends React.Component {
                   navigation.navigate('Expectation')
                 }}
               >
-                <Image source={age.image} style={s.image}/>
+                <Img source={age.image} style={s.image}/>
                 <Txt.View style={s.name} textStyle={s.nameText}>
                   {age.label}
                 </Txt.View>

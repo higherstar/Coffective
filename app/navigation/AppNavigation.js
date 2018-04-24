a77e23034950e60292130af436bf7cc7e1a78461
valentynminaev <valentin.minaev@gmail.com>
2018-04-24 17:33:50 +0300

    new changes / new screen for WIC

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { DrawerNavigator, StackNavigator, SwitchNavigator } from 'react-navigation'
+import {
+  DrawerNavigator,
+  StackNavigator,
+  SwitchNavigator
+} from 'react-navigation'
 import { TouchableOpacity } from 'react-native'
 import { AppStyles, Colors, Fonts } from '../themes'
 import { Drawer, Txt } from '../components'
@@ -25,113 +29,123 @@ import ChampionRole from '../containers/ChampionRole/ChampionRole'
 import TeamItem from '../containers/TeamItem/TeamItem'
 import SupportItem from '../containers/SupportItem/SupportItem'
 import AuthLoading from '../containers/AuthLoading/AuthLoading'
+import WicEligible from '../containers/WicEligible/WicEligible'
+
 import s from './AppNavigationStyles'
 import Icon from 'react-native-vector-icons/dist/MaterialIcons'
 
-export const BackButton = ({navigation, text}) => (
-  <TouchableOpacity
-    onPress={() => navigation.goBack()}
-    style={s.iconWrapper}
-  >
-    <Icon style={s.icon} name='chevron-left'/>
+export const BackButton = ({ navigation, text }) => (
+  <TouchableOpacity onPress={() => navigation.goBack()} style={s.iconWrapper}>
+    <Icon style={s.icon} name="chevron-left" />
     {text && <Txt style={s.backBtnText}>{text}</Txt>}
   </TouchableOpacity>
 )
 
-export const DrawerButton = ({navigation}) => (
+export const DrawerButton = ({ navigation }) => (
   <TouchableOpacity
     style={s.iconWrapper}
-    onPress={() => navigation.navigate('DrawerOpen')}
-  >
-    <Icon style={s.icon} name='menu'/>
+    onPress={() => navigation.navigate('DrawerOpen')}>
+    <Icon style={s.icon} name="menu" />
   </TouchableOpacity>
 )
 
-export const SkipButton = ({navigation, routeName}) => (
+export const SkipButton = ({ navigation, routeName }) => (
   <TouchableOpacity
     style={s.skipBtn}
-    onPress={() => navigation.navigate(routeName)}
-  >
+    onPress={() => navigation.navigate(routeName)}>
     <Txt style={s.skipBtnText}>Skip</Txt>
   </TouchableOpacity>
 )
 
-const AppStack = StackNavigator({
-  Home: {screen: Home},
-  BuildTeam: {screen: BuildTeam},
-  Faq: {screen: Faq},
-  Checklist: {screen: Checklist},
-  Article: {screen: Article},
-  HowItWorks: {screen: HowItWorks},
-  FindSupport: {screen: FindSupport},
-  Champion: {screen: Champion},
-  ChampionRole: {screen: ChampionRole},
-  TeamItem: {screen: TeamItem},
-  SupportItem: {screen: SupportItem},
-}, {
-  initialRouteName: 'Home',
-  cardStyle: {
-    backgroundColor: Colors.background,
+const AppStack = StackNavigator(
+  {
+    Home: { screen: Home },
+    BuildTeam: { screen: BuildTeam },
+    Faq: { screen: Faq },
+    Checklist: { screen: Checklist },
+    Article: { screen: Article },
+    HowItWorks: { screen: HowItWorks },
+    FindSupport: { screen: FindSupport },
+    Champion: { screen: Champion },
+    ChampionRole: { screen: ChampionRole },
+    TeamItem: { screen: TeamItem },
+    SupportItem: { screen: SupportItem },
+    WicEligible: { screen: WicEligible }
   },
-  navigationOptions: ({navigation}) => ({
-    headerStyle: AppStyles.transparentHeader,
-    headerTitleStyle: {
-      color: Colors.white,
-      fontSize: Fonts.size.h4,
-      fontWeight: '300',
+  {
+    initialRouteName: 'Home',
+    cardStyle: {
+      backgroundColor: Colors.background
     },
-    headerTintColor: Colors.white,
-    headerLeft: <BackButton navigation={navigation}/>,
-    headerRight: <DrawerButton navigation={navigation}/>,
-  }),
-})
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: AppStyles.transparentHeader,
+      headerTitleStyle: {
+        color: Colors.white,
+        fontSize: Fonts.size.h4,
+        fontWeight: '300'
+      },
+      headerTintColor: Colors.white,
+      headerLeft: <BackButton navigation={navigation} />,
+      headerRight: <DrawerButton navigation={navigation} />
+    })
+  }
+)
 
-const AuthStack = StackNavigator({
-  Login: {screen: Login},
-  Welcome: {screen: Welcome},
-  Guide: {screen: Guide},
-  ForgotPassword: {screen: ForgotPassword},
-  Register: {screen: Register},
-  PersonType: {screen: PersonType},
-  Name: {screen: Name},
-  State: {screen: State},
-  Age: {screen: Age},
-  Expectation: {screen: Expectation},
-}, {
-  initialRouteName: 'Welcome',
-  cardStyle: {
-    backgroundColor: Colors.background,
+const AuthStack = StackNavigator(
+  {
+    Login: { screen: Login },
+    Welcome: { screen: Welcome },
+    Guide: { screen: Guide },
+    ForgotPassword: { screen: ForgotPassword },
+    Register: { screen: Register },
+    PersonType: { screen: PersonType },
+    Name: { screen: Name },
+    State: { screen: State },
+    Age: { screen: Age },
+    Expectation: { screen: Expectation }
   },
-  navigationOptions: ({navigation}) => ({
-    headerStyle: AppStyles.transparentHeader,
-    headerTitleStyle: {
-      color: Colors.white,
-      fontSize: Fonts.size.h4,
-      fontWeight: '300',
+  {
+    initialRouteName: 'Welcome',
+    cardStyle: {
+      backgroundColor: Colors.background
     },
-    headerTintColor: Colors.white,
-    headerLeft: <BackButton navigation={navigation}/>,
-    headerRight: <DrawerButton navigation={navigation}/>,
-  }),
-})
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: AppStyles.transparentHeader,
+      headerTitleStyle: {
+        color: Colors.white,
+        fontSize: Fonts.size.h4,
+        fontWeight: '300'
+      },
+      headerTintColor: Colors.white,
+      headerLeft: <BackButton navigation={navigation} />,
+      headerRight: <DrawerButton navigation={navigation} />
+    })
+  }
+)
 
-const MainNav = SwitchNavigator({
-  Auth: AuthStack,
-  AuthLoading,
-  App: AppStack,
-}, {
-  initialRouteName: 'AuthLoading',
-})
+const MainNav = SwitchNavigator(
+  {
+    Auth: AuthStack,
+    AuthLoading,
+    App: AppStack
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
+)
 
-const DrawerNav = DrawerNavigator({
-  Main: {screen: MainNav},
-}, {
-  drawerWidth: 300,
-  drawerPosition: 'left',
-  contentComponent: Drawer,
-  drawerOpenRoute: 'DrawerOpen',
-  drawerCloseRoute: 'DrawerClose',
-  drawerToggleRoute: 'DrawerToggle',
-})
+const DrawerNav = DrawerNavigator(
+  {
+    Main: { screen: MainNav }
+  },
+  {
+    drawerWidth: 300,
+    drawerPosition: 'left',
+    contentComponent: Drawer,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle'
+  }
+)
 
 export default DrawerNav
